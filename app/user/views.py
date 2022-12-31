21101fc8dc51cf85419f9ff869d5c96fa5d832de
Zahra-Nasiri <z.nasir.8181@gmail.com>
2022-12-31 12:32:33 +0330

    implemented update profile endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -25,7 +25,7 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
-    authenticated_classes = [authentication.TokenAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
