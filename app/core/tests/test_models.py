c401d824e4662d622dc1d4f85b23a5216f1db39a
Zahra-Nasiri <z.nasir.8181@gmail.com>
2023-01-03 12:17:19 +0330

    Implemented Tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,7 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
-def create_user(email='user@example.com', password=testpass123):
+def create_user(email='user@example.com', password='testpass123'):
     """Create and return a new user."""
     return get_user_model().objects.create_user(email, password)
 
