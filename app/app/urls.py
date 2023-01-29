c565f2f094fc5002e44deb3aa66123effefb5bac
Zahra-Nasiri <z.nasir.8181@gmail.com>
2023-01-29 21:18:59 +0330

    Added test for recipe model changed for image field.

--- app/app/urls.py
+++ app/app/urls.py
@@ -34,8 +34,8 @@ urlpatterns = [
     path('api/recipe/', include('recipe.urls')),
 ]
 
-if settings.DEBUD:
+if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
-        document_root = settings.MEDIA_ROOT
+        document_root=settings.MEDIA_ROOT
     )
